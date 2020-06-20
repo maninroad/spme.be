f8005bfbffedf31f0bee847dbc0a85461d480bc5
Logwhen <42775157@qq.com>
2020-06-20 18:39:38 +0800

    写sql语句接口

--- src/main/java/com/spme/controller/JclController.java
+++ src/main/java/com/spme/controller/JclController.java
@@ -41,4 +41,34 @@ public class JclController {
             return ResponseEntity.status(202).body(null);
         }
     }
+    @CrossOrigin(origins = "*", allowCredentials = "true")
+    @RequestMapping(value = "/db2jcl", method = RequestMethod.POST)
+    public ResponseEntity<List<JobOutputListItem>> submitDB2JCL(@RequestBody Map<String, String> body, HttpSession session) {
+        if (AuthUtil.notLogin(session)) {
+            return ResponseEntity.status(401).body(null);
+        }
+        System.out.println(body.get("jcl"));
+        String head="//EXECSQL JOB NOTIFY=&SYSUID \n"
+                +"// SET DB2LOAD=DSNA10.SDSNLOAD  \n"
+                +"//JOBLIB  DD  DSN=&DB2LOAD,DISP=SHR  \n"
+                +"//BIND  EXEC PGM=IKJEFT01,DYNAMNBR=20 \n"
+                +"//SYSTSPRT DD SYSOUT=* \n"
+                +"//SYSPRINT DD SYSOUT=* \n"
+                +"//SYSUDUMP DD SYSOUT=* \n"
+                +"//SYSOUT   DD SYSOUT=* \n"
+                +"//SYSTSIN  DD *  \n"
+                +"   DSN SYSTEM(DP10)    \n"
+                +"   RUN  PROGRAM(DSNTIAD) PLAN(DSNTIAA1) -  \n"
+                +"    LIB('DSNA10.DP10.RUNLIB.LOAD')    \n"
+                +"//SYSIN    DD * \n"+"  ";
+        System.out.println(head+body.get("jcl"));
+        List<JobOutputListItem> res;
+        if ((res = js.submitJCL(session, head+body.get("jcl"))) != null) {
+
+            return ResponseEntity.ok(res);
+        } else {
+            // time out
+            return ResponseEntity.status(202).body(null);
+        }
+    }
 }
